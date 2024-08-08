c3a5bb3f8600e6fa603c13c207355c2108d640e0
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-08-08 13:03:46 +0200

    ejercicio 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // const wordArray = word.split('');
+    // let wordReversed = '';
+    // for(let i = wordArray.length - 1; i >= 0; i--) {
+    //     wordReversed += wordArray[i];
+    // }
+    // console.log(wordReversed);
+    // return wordReversed;
+
+    return word.split('').reverse().join('');
 
 };
+
+reverseString("hola buenas que tal");
 
 // Do not edit below this line
 module.exports = reverseString;
