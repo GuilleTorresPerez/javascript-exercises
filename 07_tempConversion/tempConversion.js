39913db17457292a06a8c4105a19728e763a6e11
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-08-08 19:12:11 +0200

    do exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5/9);
+  console.log('celsius: ' + celsius);
+  const roundedCelsius = Math.round(celsius * 10) / 10;
+  console.log('roundedCelsius: ' + roundedCelsius);
+  return roundedCelsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const fahrenheit = (degrees * (9/5)) + 32;
+  const roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
