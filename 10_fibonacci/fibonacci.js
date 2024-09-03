f921251ed203e9789bc7d55947e02d0c48aaab50
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-09-04 00:43:06 +0200

    do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if ( index < 0 ) {
+        return "OOPS"
+    }
+    
+    return fibonacciR(index)
 };
+
+function fibonacciR(i) {
+    if (i == 0) {
+        return 0
+    }
+
+    if (i == 1) {
+        return 1;
+    }
+
+    return fibonacciR(i - 1) + fibonacciR(i - 2);
+}
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
