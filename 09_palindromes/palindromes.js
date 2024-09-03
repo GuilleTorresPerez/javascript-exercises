49095db95a6837703b25d62daebf69849eef3af2
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-09-03 23:24:12 +0200

    do exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    stringAlphanumeric = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    console.log(stringAlphanumeric);
+    stringBackward = stringAlphanumeric.split("").reverse().join("");
+    console.log(stringBackward);
 
+    return stringAlphanumeric == stringBackward;
 };
+
+palindromes('Racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
